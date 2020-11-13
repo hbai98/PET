b24adc2db31aeefdec633148807cbb9bff0f4f00
Haotian Bai <164140757@qq.com>
2020-11-13 20:57:29 +0800

    file uploading

---
+++ code/Core/IO/Reader.py
@@ -0,0 +1,6 @@
+class Reader:
+    def __init__(self, path: 'The root path for all DICOM files', sub: 'bool for whether reading includes sub directories'):
+        self.path = path
+        self.sub = sub
+
+    def 
--- processing.py
+++
@@ -1,8 +0,0 @@
-
-import SimpleITK as sitk
-import radiomics
-import numpy as np
-mask_name = r"C:/Users/16414/Desktop/pet/data/ICBM_Template.zipfile/ICBM_Template.img"
-mask = sitk.ReadImage(mask_name)
-np_mask = sitk.GetArrayFromImage(mask)
-
